ab070c93bb0935e7f5aa92fa4c91b0717ed23237
winadm1n <85485845+winadm1n@users.noreply.github.com>
2024-02-28 20:03:00 +0530

    Updating code via API

--- migrated-folder/src/main/java/testmodules/SpringBootApp.java
+++ migrated-folder/src/main/java/testmodules/SpringBootApp.java
@@ -2,7 +2,7 @@ package testmodules;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.integration.config.EnableIntegration;
 @SpringBootApplication
 @EnableIntegration
 public class SpringBootApp {
