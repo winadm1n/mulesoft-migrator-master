c0e806f5cb4ef8afa6a3a84fec9fb2dc8a043a84
komal kekare <82082398+komalkekare@users.noreply.github.com>
2024-02-29 19:14:41 +0530

    adding get-user-details repo

---
+++ get-user-details/src/test/java/TestPackage/SpringBootAppTest.java
@@ -0,0 +1,13 @@
+package TestPackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
