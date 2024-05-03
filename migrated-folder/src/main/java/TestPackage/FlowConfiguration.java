c14b247225c3c5537d79da537287ce8465eb75e4
winadm1n <85485845+winadm1n@users.noreply.github.com>
2024-05-03 20:29:36 +0530

    Updating code via API

---
+++ migrated-folder/src/main/java/TestPackage/FlowConfiguration.java
@@ -0,0 +1,18 @@
+package TestPackage;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.http.dsl.Http;
+
+@Configuration
+public class FlowConfigurations {
+    @Bean
+    IntegrationFlow get_user_detailsFlow() {
+        return IntegrationFlows.from(Http.inboundGateway("/getUserDetails")).handle((p, h) -> p)
+                .handle((p, h) -> "User Name : Greg Spencer,User Address : 399 Thornall Street, 1st Floor, Edison, NJ, NJ 08837")
+                .get();
+    }
+
+   
+}
